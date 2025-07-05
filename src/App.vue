7f4be4d964b4a5fede368efb4edd26aab1b47c40
sxabc <2947261928@qq.com>
2025-07-05 17:40:02 +0800

    cicd前端项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>你好 Vue！</h1>
+    <button @click="getName">获取姓名</button>
+    <p>{{ result }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const result = ref('')
+
+const getName = async () => {
+  axios.get('/api/myapp/getName')
+  .then(res => {
+    console.log(res.data);
+    result.value = res.data
+  });
+  
+}
+</script>
